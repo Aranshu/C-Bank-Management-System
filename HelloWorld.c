4bacd4a86b9ae89e0c6454d231e49678f9daecdf
Aranshu Bansal <aranshu9399@gmail.com>
2021-03-18 11:50:34 +0530

    first commit

---
+++ HelloWorld.c
@@ -0,0 +1,17 @@
+/**
+ * @file HelloWorld.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-03-18
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World"); //printing the output'
+    return 0;
+}
