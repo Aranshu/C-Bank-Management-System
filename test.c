319a05c082af390b0e8e642211340bc94d4acb3f
Aranshu Bansal <aranshu9399@gmail.com>
2021-03-19 23:10:32 +0530

    this is a test

---
+++ test.c
@@ -0,0 +1,14 @@
+/**
+ * @file test.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-03-19
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include<stdio.h>
+int main(){
+    printf("aranshu");
+}
