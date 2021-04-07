d7cab3dee9ff91c4f36ab8a848c9cda92447d30c
Aranshu Bansal <aranshu9399@gmail.com>
2021-04-08 00:24:18 +0530

    First Comit

---
+++ 3_Implementation/src/Subtraction.c
@@ -0,0 +1,22 @@
+/**
+ * @file Subtraction.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include "Calculator.h"
+/**
+ * @brief 
+ * 
+ * @param number1 
+ * @param number2 
+ * @return int 
+ */
+ int subtraction (struct calculator* calculator_number1,struct calculator* calculator_number2)
+{
+    return calculator_number1->number-calculator_number1->number;
+}
