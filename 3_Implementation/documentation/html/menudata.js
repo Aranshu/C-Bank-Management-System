6d6dbd5aa4cc244522137a3aff45e0e4641aba9c
Aranshu Bansal <aranshu9399@gmail.com>
2021-04-16 11:56:11 +0530

    Readme Updated and Updated html document

--- 3_Implementation/documentation/html/menudata.js
+++ 3_Implementation/documentation/html/menudata.js
@@ -23,4 +23,31 @@
  @licend  The above is the entire license notice for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Main Page",url:"index.html"}]}
+{text:"Main Page",url:"index.html"},
+{text:"Data Structures",url:"annotated.html",children:[
+{text:"Data Structures",url:"annotated.html"},
+{text:"Data Structure Index",url:"classes.html"}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"Globals",url:"globals.html",children:[
+{text:"All",url:"globals.html",children:[
+{text:"a",url:"globals.html#index_a"},
+{text:"c",url:"globals.html#index_c"},
+{text:"d",url:"globals.html#index_d"},
+{text:"f",url:"globals.html#index_f"},
+{text:"g",url:"globals.html#index_g"},
+{text:"l",url:"globals.html#index_l"},
+{text:"m",url:"globals.html#index_m"},
+{text:"p",url:"globals.html#index_p"},
+{text:"s",url:"globals.html#index_s"}]},
+{text:"Functions",url:"globals_func.html",children:[
+{text:"a",url:"globals_func.html#index_a"},
+{text:"c",url:"globals_func.html#index_c"},
+{text:"d",url:"globals_func.html#index_d"},
+{text:"f",url:"globals_func.html#index_f"},
+{text:"g",url:"globals_func.html#index_g"},
+{text:"l",url:"globals_func.html#index_l"},
+{text:"m",url:"globals_func.html#index_m"},
+{text:"p",url:"globals_func.html#index_p"},
+{text:"s",url:"globals_func.html#index_s"}]},
+{text:"Typedefs",url:"globals_type.html"}]}]}]}
--- 4_TestPlanAndOutput/README.md
+++ 4_TestPlanAndOutput/README.md
@@ -5,6 +5,8 @@
 |Test ID | Description | Exp I/P | Exp O/P |
 |--------|-------------|---------|---------|
 |HLT1    | Check the calculator | Two number | One Result |
+|HLT2    | Check the accout operations | nil | nil |
+|HLT3    | Check the Login credentials | Username & Password | True/False |
 
 
 ## Table no: Low level test plan
@@ -17,6 +19,11 @@
 |LLR4   | Division |    4,2 |  2  |
 |LLR5   | Power |    2,3 |  8  |
 |LLR6   | Factorial |    5 |  120  |
-|LLR7   | Modulus |    6,2 |  3  |
+|LLR7   | Create account |  Data |  create a new account  |
+|LLR8   | Update account |  Data   |  update the selected data  |
+|LLR9   | Delete account |   Data |  Delete the account  |
+|LLR10  | Withdrawl Money |    Data |  Update Balance  |
+|LLR11  | Login Admin |   Username & Password |  True/False  |
+|LLR10  | Login Customer |   Account number |  True/False  |
 
 
